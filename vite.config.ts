b6238779a05d1d743cbd3eb4a24ae040807466c2
MayurJadhav1212 <MayurJadhav1212>
2026-05-25 07:54:03 +0530

    fix favicon

--- vite.config.ts
+++ vite.config.ts
@@ -23,16 +23,7 @@ export default defineConfig(() => {
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
-              if (id.includes('react') || id.includes('scheduler')) {
-                return 'vendor-react';
-              }
-              if (id.includes('motion') || id.includes('framer-motion')) {
-                return 'vendor-motion';
-              }
-              if (id.includes('lucide')) {
-                return 'vendor-icons';
-              }
-              return 'vendor-others';
+              return 'vendor';
             }
           },
         },
